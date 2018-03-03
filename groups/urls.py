824ef9e5f67c9c5c92eb05def126b1bc38f99af8
okprogrammmer <omk7912@gmail.com>
2018-03-03 21:13:20 +0530

    groups/urls.py

---
+++ groups/urls.py
@@ -0,0 +1,13 @@
+#groups urls.py
+
+from django.conf.urls import url
+from . import views 
+
+app_name = 'groups'
+
+urlpatterns = [
+    url(r'^$',views.ListGroups.as_view(),name='all'),
+    url(r'^new/$',views.CreateGroup.as_view(),name='create'),
+    url(r"join/(?P<slug>[-\w]+)/$",views.JoinGroup.as_view(),name="join"),
+    url(r"leave/(?P<slug>[-\w]+)/$",views.LeaveGroup.as_view(),name="leave"),
+]
